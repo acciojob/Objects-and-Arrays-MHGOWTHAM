fbaf2755fbf42405af6484aa1a885db6e70fb0aa
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-06-11 13:06:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const person = {
 };
 
 let team=players;
-let team1=players;
+let team1=team;
 let cap1=person;
 
 	
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
