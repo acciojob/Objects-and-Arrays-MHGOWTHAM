4e359aef2f59cac1109ac8d849d162ec9f29d2a3
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-06-16 11:39:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ var team = players;
 var team1 = players;
 var cap1 = person;
 
+ window.players= { 
+	 for (let i = 0; i < players.length; i++) { 
+		 players[i]=== window.team1[i]; 
+	 } 
+}
+window.players.pop(); 
+window.players!=window.team1; 
+window.players.push("Poppy"); 
+window.players!=window.team1) 
+window.person.name= window.cap1.name; 
+window.person.name = "Accio Job";
+window.cap1.age = 25; 
+window.cap1.age!=window.person.age;
 	
 window.players = team;
 window.players[3]="Dhoni"
