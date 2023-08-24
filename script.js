27fc696153a89f493d95fe68a2d9a15d674bd127
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-08-24 14:51:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,5 +7,13 @@ const person = {
 
 const team = players;
 const team1 = [...players];
-const cap1 = person;
+const cap1 = {...person};
 
+players.push("New Player");
+person.age = 81;
+
+console.log("Original players:", players);
+console.log("team:", team);
+console.log("team1:", team1);
+console.log("Original person:", person);
+console.log("cap1:", cap1);
